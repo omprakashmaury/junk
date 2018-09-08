8770ee33202cb2f362d9348bf0e5b64b26f3fa50
omprakashmaury <36072815+omprakashmaury@users.noreply.github.com>
2018-09-09 00:17:23 +0530

    Update text-wrap.py

--- hackerrank/python/strings/text-wrap.py
+++ hackerrank/python/strings/text-wrap.py
@@ -56,11 +56,8 @@
 # QRST
 # UVWX
 # YZ
-
-import textwrap
-string = input()
-k = int(input())
-print(textwrap.fill(string,k))
+def wrap(string, max_width):
+    return "\n".join([string[i:i+max_width] for i in range(0, len(string), max_width)])
 
 # without using textwrap
 # string = input().strip()
